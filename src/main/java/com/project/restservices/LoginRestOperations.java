76f2a6309462632a6fa9010a2d2b48c6293fda69
Berat Asrın CAFEROĞLU <beratasrin01@gmail.com>
2021-09-12 03:31:55 +0300

    LoginRestOperations dosyası açıldı kodlanacak

--- src/main/java/com/project/restservices/LoginRestOperations.java
+++ src/main/java/com/project/restservices/LoginRestOperations.java
@@ -1,2 +1,13 @@
-package com.project.restservices;public class LoginRestOperations {
+package com.project.restservices;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginRestOperations {
+
+
 }
